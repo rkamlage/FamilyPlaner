52cb06f7a771de63a34faca7ffae81f0fe0a5620
René Kamlage <renekamlage@googlemail.com>
2026-07-24 11:38:35 +0200

    Add PWA manifest.json and service worker for instant Android installation

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Simple Service Worker for FamilyPlaner PWA support
+const CACHE_NAME = 'familyplaner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
